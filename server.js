8bf0f932f748ba54c7d292c0b3540125d5a2519c
over40dev <ted@over40.dev>
2020-09-05 00:12:30 -0230

    setup mongoDB connection

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const transactions = require('./routes/transactions');
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const app = express();
 
