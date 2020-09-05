046ef85b01d5a1d44ada356068620225556bb452
over40dev <ted@over40.dev>
2020-09-04 23:29:42 -0230

    initial routing with controllers setup

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction,
+} = require('../controllers/transactions');
 
-router.get('/', (req, res) => res.send('Hello World!'));
+router
+  .route('/')
+  .get(getTransactions)
+  .post(addTransaction);
+
+router
+  .route('/:id')
+  .delete(deleteTransaction)
+
+// router.get('/', (req, res) => res.send('Hello World!'));
 
 module.exports = router;
 
