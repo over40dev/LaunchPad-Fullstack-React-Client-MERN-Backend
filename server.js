b630006bcbbf50d96499bcdf9adb1c0e96f62319
over40dev <ted@over40.dev>
2020-09-07 21:55:49 -0230

    MVP - MERN stack production ready

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -21,6 +22,16 @@ if (process.env.NODE_ENV === 'development') {
 
 // attach 'route' to what we want app to do when user hits this route.
 app.use('/api/v1/transactions', transactions);
+
+// insert production check for SPA below API routes (IMPORTANT)
+if (process.env.NODE_ENV === 'production' ) {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
